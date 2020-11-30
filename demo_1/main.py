fe5981d54b2cdc7f58a12adceec59898b6c4b680
DataExplorerUser <54912887+DataExplorerUser@users.noreply.github.com>
2020-11-30 21:02:03 +0100

    Add files via upload

--- demo_1/main.py
+++ demo_1/main.py
@@ -4,6 +4,7 @@ from dearpygui.simple import *
 # Main menu bar
 with window("Main"):
     add_text('Application 1')
+    add_text('Close app to continue')
     print('Application 1 running...')
 
 start_dearpygui(primary_window="Main")
